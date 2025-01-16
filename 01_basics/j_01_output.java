8463565b794c74c714a38598cede1de23b52693f
Danish Khan <dk831675@gmail.com>
2025-01-16 19:21:07 +0530

    Commit changes

---
+++ 01_basics/j_01_output.java
@@ -0,0 +1,14 @@
+// basic syntax public class {file_name}
+
+public class j_01_output{
+    public static void main(String[] args) {
+
+        //print statement output line
+        System.out.print("JAVA tutorial");
+        System.out.print("Basic syntax of Java");
+
+        //for newline we use println rather than print
+        System.out.println("This statement is for ");
+        System.out.println("New line println() ");
+    }
+}
