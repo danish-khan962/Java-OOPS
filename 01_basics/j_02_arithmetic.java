f12a36ce25d8adff57db36da2b3971449ac1274c
Danish Khan <dk831675@gmail.com>
2025-01-16 19:58:30 +0530

    Commit changes

---
+++ 01_basics/j_02_arithmetic.java
@@ -0,0 +1,12 @@
+public class j_02_arithmetic {
+    public static void main(String[] args) {
+        int var_1 = 100;
+        int var_2 = 200;
+
+        //performing arithmetic
+        System.out.println("Addition :-> " + (var_1 + var_2));
+        System.out.println("Subtraction :-> " + (var_1 - var_2));
+        System.out.println("Multiplication :-> " + (var_1 * var_2));
+        System.out.println("Divison :-> " + (var_1 / var_2));
+    }
+}
